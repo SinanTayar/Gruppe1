8904b33fb2f746d54697e04287e3714fce6bf8a4
Sinan-Tayar <st071@hdm-stuttgart.de>
2022-11-03 21:56:37 +0100

    init

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from. import views
+
+urlpatterns=[
+path("<int:id>",views.index, name="index"),
+path("", views.home, name="home")
+]
---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+
+# Create your views here.
+def index(response,id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html",{"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html",{})
